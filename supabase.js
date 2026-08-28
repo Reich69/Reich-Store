b6dfc417941cf3729a3b90cb295756789fe1c262
Reich <rokonuu11230@gmail.com>
2026-08-28 20:10:22 +0600

    Add Supabase connection

---
+++ supabase.js
@@ -0,0 +1,8 @@
+const SUPABASE_URL = "https://ssutrajdxlfutqcvlgsq.supabase.co";
+
+const SUPABASE_KEY = "sb_publishable_u1-sWpGD4iSu2HkhSu6ISg_1JzGY8ca";
+
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
